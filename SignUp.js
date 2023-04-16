ea63d3efeca338456f98ce2b9dbf49a70dfc5044
Chau-Elena <elenachau@nevada.unr.edu>
2023-04-16 03:34:48 -0700

    edit

---
+++ SignUp.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import '../../App.css';
+import Account from '../Account';
+
+export default function SignUp() {
+  return (
+    <>
+        <h1 className='sign-up'>JOIN THE COMMUNITY</h1>;
+    </>
+  );
+}
