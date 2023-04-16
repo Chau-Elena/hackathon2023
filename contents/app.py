4a4b985560ef0db86445a27b7e1526985dbc2140
Muniz-Miguel <112589409+Muniz-Miguel@users.noreply.github.com>
2023-04-16 01:46:09 -0700

    trash_api.py now works under app.py

--- contents/app.py
+++ contents/app.py
@@ -1,6 +1,9 @@
 from flask import Flask, render_template, request
+from werkzeug.utils import secure_filename
+from roboflow import Roboflow
 import config
 import gptcontents as gpt
+import os
 # import os
 
 # APP_PATH = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -13,11 +16,42 @@ def page_not_found(e):
 app = Flask(__name__)
 app.config.from_object(config.config['development'])
 app.register_error_handler(404, page_not_found)
+rf = Roboflow(api_key="QgwRoa71JYqIgWfVPa8k")
+project = rf.workspace().project("trashdetection1141")
+model = project.version(1).model
 
 
 @app.route('/', methods=["GET", "POST"])
 def index():
     return render_template('index.html', **locals())
+
+@app.route('/upload', methods=['GET', 'POST'])
+def upload_image():
+    # Check if a file was uploaded
+    if request.method == 'POST' and 'file' in request.files:
+        # Get the uploaded file
+        file = request.files['file']
+        
+        # Save the file to disk
+        filename = secure_filename(file.filename)
+        file.save(filename)
+        
+        # Call the model to get the predicted classes
+        result = model.predict(filename, confidence=30, overlap=30)
+        data = result.json()
+        imageclass = []
+        for prediction in data['predictions']:
+            class_name = prediction['class']
+            imageclass.append(class_name)
+        
+        # Remove the saved file
+        os.remove(filename)
+        
+        # Return the predicted classes as a response
+        return '\n'.join(imageclass)
+    
+    # If no file was uploaded, render the predict page
+    return render_template('404.html')
 
 # app route
 
