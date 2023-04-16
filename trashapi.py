e66710daca7afc6fdfbcf437983b4608dee20863
Muniz-Miguel <112589409+Muniz-Miguel@users.noreply.github.com>
2023-04-15 22:48:48 -0700

    Made api json data parseable

--- trashapi.py
+++ trashapi.py
@@ -4,8 +4,47 @@ project = rf.workspace().project("trashdetection1141")
 model = project.version(1).model
 
 # infer on a local image
-print(model.predict("WIN_20230415_18_26_04_Pro.jpg", confidence=30, overlap=30).json())
+#print(model.predict("WIN_20230415_18_26_04_Pro.jpg", confidence=30, overlap=30).json())
+# interpret the results
+# Call the predict function and get the result object
+result = model.predict("twogirlsonecup.png", confidence=30, overlap=30)
 
+# Parse the JSON data into a Python dictionary or list
+data = result.json()
+print (len(data['predictions']))
+
+for prediction in data['predictions']:
+    class_name = prediction['class']
+    list
+    print(class_name)
+
+
+
+# predictions = data['predictions']
+
+# print(data)
+
+# class_name = data['predictions'][0]['class']
+# print(class_name)
+
+# for prediction in predictions:
+#     class_name = predictions['class']
+#     print(class_name)
+
+
+
+# print(data["predictions"])
+
+# if len(data) == 0:
+#     print("Error: No objects detected in image.")
+# else:
+#     # Extract the value of the 'class' key from the first dictionary in the list
+#     if 'class' not in data[0]:
+#         print("Error: 'class' key not found in dictionary.")
+#     else:
+#         class_name = data[0]['class']
+#         # Use the class name as needed, for example:
+#         print(class_name)
 # visualize your prediction
 # model.predict("your_image.jpg", confidence=40, overlap=30).save("prediction.jpg")
 
